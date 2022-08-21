4905036bc7bb748c96a7abe97ab97a06216309b5
moisesrp2021 <46361656+master-hit@users.noreply.github.com>
2022-08-21 01:25:17 -0400

    updating ide and mf

--- ide.fish
+++ ide.fish
@@ -1,10 +1,14 @@
 function ide
-    set -l name (basename $argv[1])
-    echo "provided path:    " $argv[1] 
-    echo "Project:      " $name
-    cd $today
-    tmux renamew [NOTES]
-    set current_project $argv[1]
-    tmux new-window -n [SERVER] -c $argv[1]
-    tmux new-window -n [app] -c $argv[1]
+    if test -d $argv[1]
+        set -l name (basename $argv[1])
+        echo "provided path:    " $argv[1] 
+        echo "Project:      " $name
+        cd $today
+        tmux renamew [NOTES]
+        set current_project $argv[1]
+        tmux new-window -n [SERVER] -c $argv[1]
+        tmux new-window -n [app] -c $argv[1]
+    else
+        echo argument must be a path to a project
+    end
 end
--- mf.fish
+++ mf.fish
@@ -1,4 +1,4 @@
-function mf
+function mf --description 'create directory and files inside'
     mkdir -p $argv[1]
     for i in $argv[2..-1];
         touch $argv[1]/$i;
